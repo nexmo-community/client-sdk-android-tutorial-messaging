e96cae9b641fa5b0f0dc58dd819c06e7787bf7bd
igor.wojda <igor.wojda@gmail.com>
2020-04-03 08:09:36 +0200

    updated formatting

--- kotlin-completed/app/src/main/java/com/vonage/tutorial/messaging/Config.kt
+++ kotlin-completed/app/src/main/java/com/vonage/tutorial/messaging/Config.kt
@@ -7,8 +7,14 @@ data class User(
 
 object Config {
 
-    const val CONVERSATION_ID: String = ""
+    const val CONVERSATION_ID: String = "CON-58ea5d7c-d0db-494a-af82-823b5c7e8b82"
 
-    val jane = User("Jane", "")
-    val joe = User("Joe", "")
+    val jane = User(
+        "Jane",
+        ""
+    )
+    val joe = User(
+        "Joe",
+        ""
+    )
 }
